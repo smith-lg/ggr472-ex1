ac199030c307ae708b2cfc7cb8815aae99377c9a
smith-lg <88113647+smith-lg@users.noreply.github.com>
2023-01-17 20:44:36 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var button = document.getElementById("my-btn");
+
+button.addEventListener("click", function(){
+    alert("You clicked me!");
+});
+
+/*
+button.onclick = function(){
+    alert("You clicked me!");
+}
+
+function clicked() {
+    console.log("You clicked me")
+}
+*/
